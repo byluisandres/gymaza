18c9d2bbab83675445b046a29ca2d6a8ab1cef9a
Luis andrés bolanos <luisandres@bolanos>
2021-01-11 16:40:53 +0100

    Inscripción realizada

--- src/app/components/inscripcion/inscripcion.component.ts
+++ src/app/components/inscripcion/inscripcion.component.ts
@@ -3,7 +3,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { Cliente } from 'src/app/models/cliente';
 import { Inscripcion } from 'src/app/models/inscripcion';
 import { Precio } from 'src/app/models/precio';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-inscripcion',
   templateUrl: './inscripcion.component.html',
@@ -11,7 +11,9 @@ import { Precio } from 'src/app/models/precio';
 export class InscripcionComponent implements OnInit {
   inscripcion: Inscripcion = new Inscripcion();
   clienteSeleccionado: Cliente = new Cliente();
+  precioSeleccionado = new Precio();
   precios: Precio[] = new Array<Precio>();
+  selectPrecio: string = 'null';
   constructor(private db: AngularFirestore) {}
 
   ngOnInit(): void {
@@ -36,7 +38,100 @@ export class InscripcionComponent implements OnInit {
     this.clienteSeleccionado = new Cliente();
     this.inscripcion.cliente = undefined;
   }
+
+  seleccionarPrecio(precioId: string) {
+    this.precioSeleccionado = this.precios.find(
+      (precio) => precio.id == precioId
+    );
+
+    this.inscripcion.precio = this.precioSeleccionado.ref;
+
+    this.inscripcion.fecha = new Date();
+
+    this.inscripcion.subTotal = this.precioSeleccionado.precio;
+    this.inscripcion.iva = this.inscripcion.subTotal * 0.21;
+    this.inscripcion.total = this.inscripcion.subTotal + this.inscripcion.iva;
+    /*dia =1
+    semana=2
+    mes=3
+    año=4
+    */
+
+    if (this.precioSeleccionado.tipoDuracion === 1) {
+      var dias: number = this.precioSeleccionado.duracion;
+      let fechaFinal = new Date(
+        this.inscripcion.fecha.getFullYear(),
+        this.inscripcion.fecha.getMonth(),
+        this.inscripcion.fecha.getDate() + dias
+      );
+      this.inscripcion.fechaFinal = fechaFinal;
+    }
+    if (this.precioSeleccionado.tipoDuracion === 2) {
+      var dias: number = this.precioSeleccionado.duracion * 7;
+      let fechaFinal = new Date(
+        this.inscripcion.fecha.getFullYear(),
+        this.inscripcion.fecha.getMonth(),
+        this.inscripcion.fecha.getDate() + dias
+      );
+      this.inscripcion.fechaFinal = fechaFinal;
+    }
+    if (this.precioSeleccionado.tipoDuracion === 3) {
+      var meses: number = this.precioSeleccionado.duracion;
+      let fechaFinal = new Date(
+        this.inscripcion.fecha.getFullYear(),
+        this.inscripcion.fecha.getMonth() + meses,
+        this.inscripcion.fecha.getDate()
+      );
+      this.inscripcion.fechaFinal = fechaFinal;
+    }
+    if (this.precioSeleccionado.tipoDuracion === 4) {
+      var dias: number = this.inscripcion.fecha.getDate();
+      var meses: number =
+        this.precioSeleccionado.duracion + this.inscripcion.fecha.getMonth();
+      var anio: number =
+        this.inscripcion.fecha.getFullYear() + this.precioSeleccionado.duracion;
+      let fechaFinal = new Date(anio, meses, dias);
+      this.inscripcion.fechaFinal = fechaFinal;
+    }
+  }
+
   guardar() {
-    console.log(this.inscripcion);
+    if (this.inscripcion.validar().esValido) {
+      let inscripcinUsuario = {
+        fecha: this.inscripcion.fecha,
+        fechaFinal: this.inscripcion.fechaFinal,
+        cliente: this.inscripcion.cliente,
+        precio: this.inscripcion.precio,
+        subTotal: this.inscripcion.subTotal,
+        iva: this.inscripcion.iva,
+        total: this.inscripcion.total,
+      };
+      this.db
+        .collection('inscripciones')
+        .add(inscripcinUsuario)
+        .then((resultado) => {
+          this.inscripcion = new Inscripcion();
+          this.clienteSeleccionado = new Cliente();
+          this.precioSeleccionado = new Precio();
+          this.selectPrecio = 'null';
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 1500,
+            timerProgressBar: true,
+          });
+
+          Toast.fire({
+            icon: 'success',
+            title: 'Se ha inscripto con exito',
+          });
+        });
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: this.inscripcion.validar().mensaje,
+      });
+    }
   }
 }
--- src/app/models/inscripcion.ts
+++ src/app/models/inscripcion.ts
@@ -9,12 +9,57 @@ export class Inscripcion {
   iva: number;
   total: number;
   constructor() {
-    this.fecha = this.fecha;
-    this.fechaFinal = this.fechaFinal;
+    this.fecha = null;
+    this.fechaFinal = null;
     this.cliente = this.cliente;
     this.precio = this.precio;
-    this.subTotal = this.subTotal;
-    this.iva = this.iva;
-    this.total = this.total;
+    this.subTotal = null;
+    this.iva = null;
+    this.total = null;
+  }
+  validar(): any {
+    let respuesta = {
+      esValido: false,
+      mensaje: '',
+    };
+
+    //válidaciones
+    if (this.cliente === null || this.cliente == undefined) {
+      respuesta.esValido = false;
+      respuesta.mensaje = 'Seleccione un cliente';
+      return respuesta;
+    }
+    if (this.precio === null || this.precio == undefined) {
+      respuesta.esValido = false;
+      respuesta.mensaje = 'Seleccione un precio';
+      return respuesta;
+    }
+    if (this.fecha === null || this.fecha == undefined) {
+      respuesta.esValido = false;
+      respuesta.mensaje = 'Seleccione fecha de inicio';
+      return respuesta;
+    }
+    if (this.fechaFinal === null || this.fechaFinal == undefined) {
+      respuesta.esValido = false;
+      respuesta.mensaje = 'Seleccione fecha final';
+      return respuesta;
+    }
+    if (this.subTotal <= 0 || this.subTotal == undefined) {
+      respuesta.esValido = false;
+      respuesta.mensaje = 'No se ha podido calcular el subtotal';
+      return respuesta;
+    }
+    if (this.iva <= 0 || this.iva == undefined) {
+      respuesta.esValido = false;
+      respuesta.mensaje = 'No se ha podido calcular el iva';
+      return respuesta;
+    }
+    if (this.total <= 0 || this.total == undefined) {
+      respuesta.esValido = false;
+      respuesta.mensaje = 'No se ha podido calcular eltotal';
+      return respuesta;
+    }
+    respuesta.esValido = true;
+    return respuesta;
   }
 }
